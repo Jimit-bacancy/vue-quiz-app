56debcc1003cf5b08005bd0793367cfbb7e8c5ad
Jimit-bacancy <jimit.shah@bacancy.com>
2022-02-04 13:43:50 +0530

    Show correct answers in quiz.

--- src/App.vue
+++ src/App.vue
@@ -16,13 +16,15 @@ export default {
 </script>
 
 <style>
+* {
+  box-sizing: border-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin: 0;
   line-height: 1.6;
 }
 </style>
--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -1,16 +1,15 @@
 <template>
-  <div>
-    <h1>Quiz</h1>
-    <div v-html="loading ? `Loading ...` : currentQuestion.question"></div>
+  <div class="container">
+    <h1 v-html="loading ? 'Loading...' : currentQuestion.question"></h1>
     <!-- Only first question is displayed -->
     <form v-if="currentQuestion">
       <button
         v-for="answer in currentQuestion.answers"
+        :index="currentQuestion.key"
         :key="answer"
-        :questionIndex="currentQuestion.key"
         v-html="answer"
-      >
-      </button>
+        @click.prevent="handleClick"
+      ></button>
     </form>
   </div>
 </template>
@@ -20,14 +19,14 @@ export default {
   data() {
     return {
       questions: [],
-      loading: false,
-      questionIndex: 0,
+      loading: true,
+      index: 0,
     };
   },
   computed: {
     currentQuestion() {
       if (this.questions !== []) {
-        return this.questions[this.questionIndex];
+        return this.questions[this.index];
       }
       return null;
     },
@@ -35,21 +34,84 @@ export default {
   methods: {
     async fetchQuestions() {
       this.loading = true;
-
       //fetching questions from api
       let response = await fetch(
-        "https://opentdb.com/api.php?amount=20&category=21&type=multiple"
+        "https://opentdb.com/api.php?amount=5&category=21&type=multiple"
       );
+      let index = 0; //To identify single answer
       let data = await response.json();
       let questions = data.results.map((question) => {
         question.answers = [
           question.correct_answer,
           ...question.incorrect_answers,
         ];
+        //shuffle above array
+        for (let i = question.answers.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          [question.answers[i], question.answers[j]] = [
+            question.answers[j],
+            question.answers[i],
+          ];
+        }
+        //add right answers and key
+        question.rightAnswer = null;
+        question.key = index;
+        index++;
         return question;
       });
       this.questions = questions;
       this.loading = false;
+    },
+
+    handleClick(e) {
+      let index = e.target.getAttribute("index");
+      let pollutedUserAnswer = e.target.innerHTML; // innerHTML is polluted with decoded HTML entities e.g ' from &#039;
+      /* Clear from pollution with ' */
+      let userAnswer = pollutedUserAnswer.replace(/'/, "&#039;");
+      //set answer
+      this.questions[index].userAnswer = userAnswer;
+      /* Set class "clicked" on button with userAnswer -> for CSS Styles; Disable other sibling buttons */
+      e.target.classList.add("clicked");
+      let allButtons = document.querySelectorAll(`[index="${index}"]`);
+
+      for (let i = 0; i < allButtons.length; i++) {
+        if (allButtons[i] === e.target) continue;
+
+        allButtons[i].setAttribute("disabled", "");
+      }
+      this.checkCorrectAnswer(e, index);
+    },
+
+    checkCorrectAnswer(e, index) {
+      let question = this.questions[index];
+      if (question.userAnswer) {
+        if (this.index < this.questions.length - 1) {
+          setTimeout(
+            function () {
+              this.index += 1;
+            }.bind(this),
+            3000
+          );
+        }
+        if (question.userAnswer === question.correct_answer) {
+          /* Set class on Button if user answered right, to celebrate right answer with animation joyfulButton */
+          e.target.classList.add("rightAnswer");
+          /* Set rightAnswer on question to true, computed property can track a streak out of 20 questions */
+          this.questions[index].rightAnswer = true;
+        } else {
+          /* Mark users answer as wrong answer */
+          e.target.classList.add("wrongAnswer");
+          this.questions[index].rightAnswer = false;
+          /* Show right Answer */
+          let correctAnswer = this.questions[index].correct_answer;
+          let allButtons = document.querySelectorAll(`[index="${index}"]`);
+          allButtons.forEach(function (button) {
+            if (button.innerHTML === correctAnswer) {
+              button.classList.add("showRightAnswer");
+            }
+          });
+        }
+      }
     },
   },
   mounted() {
@@ -59,8 +121,93 @@ export default {
 </script>
 
 <style scoped>
+.container {
+  margin: 1rem auto;
+  padding: 1rem;
+  max-width: 750px;
+}
+form {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+}
 button {
-    margin-left: 10px;
-    margin-top: 10px;
+  font-size: 1.1rem;
+  box-sizing: border-box;
+  padding: 1rem;
+  margin: 0.5rem;
+  width: 40%;
+  background-color: rgba(100, 100, 100, 0.3);
+  border: none;
+  border-radius: 0.4rem;
+  box-shadow: 3px 5px 5px rgba(0, 0, 0, 0.2);
+}
+button:hover:enabled {
+  transform: scale(1.02);
+  box-shadow: 0 3px 3px 0 rgba(0, 0, 0, 0.14), 0 1px 7px 0 rgba(0, 0, 0, 0.12),
+    0 3px 1px -1px rgba(0, 0, 0, 0.2);
+}
+button:focus {
+  outline: none;
+}
+button:active:enabled {
+  transform: scale(1.05);
+}
+
+@keyframes flashButton {
+  0% {
+    opacity: 1;
+    transform: scale(1.01);
+  }
+  50% {
+    opacity: 0.7;
+    transform: scale(1.02);
+  }
+  100% {
+    opacity: 1;
+    transform: scale(1);
+  }
+}
+
+button.clicked {
+  pointer-events: none;
+}
+
+button.rightAnswer {
+  animation: flashButton;
+  animation-duration: 700ms;
+  animation-delay: 200ms;
+  animation-iteration-count: 3;
+  animation-timing-function: ease-in-out;
+  color: black;
+  background: linear-gradient(
+    210deg,
+    rgba(0, 178, 72, 0.25),
+    rgba(0, 178, 72, 0.5)
+  );
+}
+
+button.wrongAnswer {
+  color: black;
+  background: linear-gradient(
+    210deg,
+    rgba(245, 0, 87, 0.25),
+    rgba(245, 0, 87, 0.5)
+  );
+}
+
+button.showRightAnswer {
+  animation: flashButton;
+  animation-duration: 700ms;
+  animation-delay: 200ms;
+  animation-iteration-count: 2;
+  animation-timing-function: ease-in-out;
+  color: black;
+  background: linear-gradient(
+    210deg,
+    rgba(0, 178, 72, 0.25),
+    rgba(0, 178, 72, 0.5)
+  );
 }
 </style>
